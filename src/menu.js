992cef145b68576ea3bbdae614c50cd320934b91
Priyanka <priyanka.kumari@hyperisland.se>
2024-11-20 11:45:56 +0100

    Update source files

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,14 @@
 // Importing images used for each menu item
 import signUpImage from "./images/Sign-up.png";
-import pastaImage from "./images/pasta.jpg";
-import saladImage from "./images/salad.jpg";
-import pizzaImage from "./images/pizza.jpg";
+import etchasketchImage from "./images/Etch-a-sketch.png";
+import tictactoeImage from "./images/tictactoe.png";
+import restaurantImage from "./images/restaurant_image.png";
+
+import weatherappImage from "./images/WeatherApp.png";
+import todolistImage from "./images/To-do list.png";
+import libraryImage from "./images/library.png";
+import calculatorImage from "./images/calculator.png";
+
 
 /* Generates the content for the Menu Page */
 function createMenuPage() {
@@ -11,11 +17,10 @@ function createMenuPage() {
 
   // Define the inner HTML structure for the menu page
   menuDiv.innerHTML = `
-    
-      
-      <section class="portfolio-section">
+    <section class="portfolio-section">
         <h2 class="portfolio-title">Portfolio</h2>
         <div class="portfolio-container">
+
           <!-- Card 1 -->
           <div class="portfolio-card">
             <img src="${signUpImage}" alt="Project 1" class="portfolio-image" width="300px" height="200px">
@@ -27,27 +32,30 @@ function createMenuPage() {
             </div>
           </div>
           <!-- Card 2 -->
+
           <div class="portfolio-card">
-            <img src="" alt="Project 2" class="portfolio-image">
+            <img src="${etchasketchImage}" alt="Project 2" class="portfolio-image">
             <div class="portfolio-content">
               <h3>Etch-a-Sketch project</h3>
               <p>It is a fun and interactive browser-based drawing tool. <br>Key concepts: customizable grid, HTML, CSS, JavaScript Skills</p>
-              <a href="https://github.com/prinsergioprin/etch-a-sketch-prinsergioprin" class="portfolio-button">Go somewhere</a>
+              <a href="https://github.com/prinsergioprin/etch-a-sketch-prinsergioprin" class="portfolio-button">Go To Project</a>
             </div>
           </div>
+
           <!-- Card 3 -->
           <div class="portfolio-card">
-            <img src="" alt="Project 3" class="portfolio-image">
+            <img src="${tictactoeImage}" alt="Project 3" class="portfolio-image">
             <div class="portfolio-content">
               <h3>Tic Tac Toe Project</h3>
               <p>It is a classic implementation of the popular two-player strategy game.<br>Key concepts: DOM manipulation using JavaScript, HTML, and CSS</p>
-              <a href="#" class="portfolio-button">Go somewhere</a>
+              <a href="#" class="portfolio-button">Go To Project</a>
             </div>
           </div>
+
            <!-- Card 4 -->
         <div class="portfolio-card">
           <img
-            src=""
+            src="${restaurantImage}"
             alt="Project 3"
             class="portfolio-image"
           />
@@ -60,10 +68,11 @@ function createMenuPage() {
             <a href="#" class="portfolio-button">Go to Project</a>
           </div>
         </div>
+
         <!-- Card 5 -->
         <div class="portfolio-card">
           <img
-            src=""
+            src="${weatherappImage}"
             alt="Project 3"
             class="portfolio-image"
           />
@@ -80,10 +89,11 @@ function createMenuPage() {
             >
           </div>
         </div>
+
         <!-- Card 6 -->
         <div class="portfolio-card">
           <img
-            src=""
+            src="${todolistImage}"
             alt="Project 3"
             class="portfolio-image"
           />
@@ -100,10 +110,11 @@ function createMenuPage() {
             >
           </div>
         </div>
+
         <!-- Card 7 -->
         <div class="portfolio-card">
           <img
-            src=""
+            src="${libraryImage}"
             alt="Project 3"
             class="portfolio-image"
           />
@@ -118,10 +129,11 @@ function createMenuPage() {
             >
           </div>
         </div>
+
         <!-- Card 8 -->
         <div class="portfolio-card">
           <img
-            src=""
+            src="${calculatorImage}"
             alt="Project 3"
             class="portfolio-image"
           />
